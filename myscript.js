70f91c9b44b19b123f9112ca7a781b2d245efe68
Z4Tech <zhangchenguang1994@gmail.com>
2015-09-18 17:00:24 +0800

    find the report

--- myscript.js
+++ myscript.js
@@ -1,1 +1,15 @@
-alert("Hello!");
+var frame = $('frame[name="main"]', top.document);
+frame.on("load", function(){
+  var frameDocument = frame[0].contentDocument;
+  console.log(frameDocument);
+  var jDoc = $(frameDocument);
+  console.log(jDoc.find('title').html());
+  if(jDoc.find('title').html()=='成绩单'){
+    if(jDoc.find('body').children().eq(1)
+    .children().html() ==
+    "注意：申请办理成绩单的学生，请于提交申请之后尽快在本页面的最下方进行成绩单的确认。确认之后，已支付的成绩单办理申请方可生效。"){
+      alert("这是成绩单！");
+    }
+    // $(frameDocument).find("body").html("<p>蛤蛤</p>");
+  }
+});
